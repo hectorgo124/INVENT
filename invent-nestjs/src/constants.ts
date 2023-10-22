84a30303dc9f0e7cace1ef303c9623cc69390097
hectorgo124 <hgranelloliva@gmail.com>
2023-10-22 20:07:10 +0200

    add seed to db

--- invent-nestjs/src/constants.ts
+++ invent-nestjs/src/constants.ts
@@ -2,5 +2,5 @@ export const DATABASE_USER = 'root';
 export const DATABASE_NAME = 'invent';
 export const DATABASE_HOST = 'localhost';
 export const DATABASE_PORT = 3306;
-export const ORIGIN_CORS = 'localhost:4200';
+export const ORIGIN_CORS = 'http://localhost:4200';
 export const LISTEN_PORT = 3000;
